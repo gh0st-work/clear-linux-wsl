e51efa511ae5ee0b8e370d3839f6483e58c2c72f
gh0st <59336046+gh0st-work@users.noreply.github.com>
2024-08-09 12:35:21 +0300

    Fix

--- get_latest_rootfs.sh
+++ get_latest_rootfs.sh
@@ -59,7 +59,7 @@ main() {
 
     echo "- Creating tarball..."
     cd $copy_name
-    export XZ_OPT='-$xz_level -T$xz_threads'
+    export XZ_OPT="-$xz_level -T$xz_threads"
     sudo -E tar -cJf ../clear_linux_rootfs.tar.xz * || exit 1
     cd ..
     sudo rm -rf $copy_name || exit 1
